80c0873f911b05a45f6665920a272f248e958172
vijay9711 <vijaysm97@gmail.com>
2024-12-23 17:27:21 -0600

    test repo

--- src/containers/Trending/Trending.js
+++ src/containers/Trending/Trending.js
@@ -147,14 +147,3 @@ class Trending extends Component {
 }
 
 export default Trending;
-
-{/* <div key={index} onClick={(event) => this.getSelectedMovie(event, movies, index)} className="group bg-primary  text-white rounded-xl shadow:lg border-1 overflow-hidden m-5 cursor-pointer hover:shadow-mainColorShadow duration-500">
-                                            <img className="w-full rounded-lg" src={process.env.REACT_APP_IMAGE_URL + movies.poster_path} alt="poster" />
-                                            <div className="px-5 py-3">
-                                                <ThemeProvider theme={this.getRatingBG(movies.vote_average)}>
-                                                    <Rating id={index + 'rating'} />
-                                                </ThemeProvider>
-                                                <span className="text-lg float-right font-bold text-white  group-hover:text-xl group-hover:font-bold">{(movies.vote_average.toFixed(1))}/10</span>
-                                            </div>
-                                            <p className="px-5 py-2 font-bold text-lg truncate text-white  group-hover:text-xl group-hover:font-bold" >{movies.original_title}</p>
-                                        </div> */}
